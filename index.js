911b4abdd8b81249ed0fe60d556a94f483460da9
olijen <olijenius@gmail.com>
2016-04-26 00:47:20 +0300

    #1 - add bootstrap design. Add "Add" button. Show\hide it. Refactor controller. Add count.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-angular.module('cert', [])
-  .controller('MainController', function() {
 
-    var main = this; 
+//Main controller
+function MainController() {
+  var main = this; 
 
     main.items = localStorage;
 
@@ -21,30 +21,19 @@ angular.module('cert', [])
     main.show = function(key)
     {
       //Parse item
-      var asn1 = ASN1.decode(main.items[key]);
-      var oidsValues = getOIDValues(asn1);
-      console.log(oidsValues);
+      var asn1 = ASN1.decode(main.items[key]),
+          oidsValues = getOIDValues(asn1);
+      //Reload model
       angular.forEach(main.currentParsedItem, function(value, key) {
-        console.log(key);
         if (oidsValues[key] !== undefined)
           main.currentParsedItem[key] = oidsValues[key];
         else main.currentParsedItem[key] = null;
       });
     }
+}
 
-    main.dragOptions = {
-      start: function(e) {
-        event.preventDefault();
-        console.log("STARTING");
-      },
-      drag: function(e) {
-        event.preventDefault();
-        console.log("DRAGGING");
-      },
-      stop: function(e) {
-        event.preventDefault();
-        console.log("STOPPING");
-      },
-      container: 'dd'
-    }
-  });
+//---INIT
+
+angular
+  .module('cert', [])
+  .controller('MainController', MainController);
