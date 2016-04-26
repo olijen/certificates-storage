0ee0818b4cfd781603c5797d61676919cef0e026
olijen <olijenius@gmail.com>
2016-04-26 18:35:43 +0300

    #5 - directive - onReadFile. Replace logic of upload and parse .cer. Rewrite logic of storage binary data - serialize to JSON.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@
 function MainController() {
     var main = this;
 
-    main.items = localStorage;
+    main.items = JSON.parse(window.localStorage.getItem('cerf')) || {};
 
     main.currentParsedItem = {
-        commonName: null,
-        countryName: null,
-        localityName: null,
-        organizationName: null,
-        serialNumber: null,
+        commonName:         null,
+        countryName:        null,
+        localityName:       null,
+        organizationName:   null,
+        serialNumber:       null,
     };
 
     main.addCertificate = function () {
@@ -26,11 +26,74 @@ function MainController() {
                 main.currentParsedItem[key] = oidsValues[key];
             else main.currentParsedItem[key] = null;
         });
-    }
+    };
+
+    main.uploadEnd = function($cert) {
+        main.items[$cert.name] = $cert.result;
+    };
+}
+
+//---DIRECTIVE
+
+function onReadFile($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+            element.on('drop', function(event) {
+                event.preventDefault();
+                var i = 0,
+                    files = event.dataTransfer.files,
+                    len = files.length;
+
+                for (; i < len; i++) {
+                    var file = files[i];
+
+                    /*cl("Filename: " + file.name); cl("Type: " + file.type);  cl("Size: " + file.size + " bytes");*/
+
+                    var reader = new FileReader();
+                    reader.onload = (function (theFile) {
+                        return function (e) {
+                            var result     = e.target.result,
+                                asn1       = ASN1.decode(result),
+                                oidsValues = getOIDValues(asn1),
+                                name       = prompt('Upload with success. Enter certificate name', oidsValues.commonName),
+                                cerf       = JSON.parse(window.localStorage.getItem('cerf')) || {};
+                            cerf[name] = result;
+                            window.localStorage.setItem('cerf',
+                                JSON.stringify(cerf)
+                            );
+                            scope.$apply(function() {
+                                fn(scope, {$cert: {name:name, result:result} });
+                            });
+                            //TODO:
+                            //var view  = new Uint8Array(result);
+                            //console.log(view);
+                        };
+                    })(file);
+
+                    reader.onerror = function (event) {
+                        console.error("Cant read file! code: " + event.target.error.code);
+                    };
+                    //TODO:
+                    //reader.readAsArrayBuffer(file);
+                    reader.readAsBinaryString(file);
+                }
+            });
+
+            element.on('dragover', function(event) {
+                event.preventDefault();
+            });
+        }
+    };
 }
 
 //---INIT
 
 angular
     .module('cert', [])
-    .controller('MainController', MainController);
+    .controller('MainController', MainController)
+    .directive('onReadFile', onReadFile)
+;
--- js/parse.js
+++ js/parse.js
@@ -1,52 +1,6 @@
 function cl(t) {
     console.log(t);
 }
-var target = document.getElementById("file-upload");
-
-target.addEventListener("dragover", function (event) {
-    event.preventDefault();
-}, false);
-
-target.addEventListener("drop", function (event) {
-    event.preventDefault();
-    var i = 0,
-        files = event.dataTransfer.files,
-        len = files.length;
-
-    for (; i < len; i++) {
-        var file = files[i];
-
-         /*cl("Filename: " + file.name); cl("Type: " + file.type);  cl("Size: " + file.size + " bytes");*/
-
-        var reader = new FileReader();
-        reader.onload = (function (theFile) {
-            return function (e) {
-                var result = e.target.result,
-                    asn1 = ASN1.decode(result),
-                    oidsValues = getOIDValues(asn1);
-
-                name = prompt('Upload with success. Enter certificate name', oidsValues.commonName);
-                window.localStorage.setItem(name, result);
-                //Angular-way =)
-                var el = document.getElementById('main');
-                var scope = angular.element(el).scope();
-                scope.$apply(function () {
-                    scope.items[name] = result;
-                });
-                //TODO:
-                //var view  = new Uint8Array(result);
-                //console.log(view);
-            };
-        })(file);
-
-        reader.onerror = function (event) {
-            console.error("Cant read file! code: " + event.target.error.code);
-        };
-        //TODO:
-        //reader.readAsArrayBuffer(file);
-        reader.readAsBinaryString(file);
-    }
-}, false);
 
 function getOIDValues(asn) {
     var oidsValues = [],
